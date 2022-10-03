8bdd10746d77d8a00d10c59fa102743060fa2d14
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-10-02 20:53:19 -0700

    added id to textarea in singlepost.handlebars

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,7 +1,7 @@
 async function commentFormHandler(event) {
     event.preventDefault();
 
-    const comment_text = document.querySelector('textarea[name="commment-body"]').value.trim();
+    const comment_text = document.querySelector('#comment-text').value.trim();
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
