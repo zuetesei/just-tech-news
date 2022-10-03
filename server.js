8b17ec8271660e771ddac4ede8906d8015e9a789
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-10-03 11:11:31 -0700

    used helper functions to format strings and consolidated HTML code with partials

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
